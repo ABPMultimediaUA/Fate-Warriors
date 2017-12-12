794f5dc89fd6ff2e99e36f8eefe1111f64285694
Unknown <manumeq@gmail.com>
2017-12-12 16:09:02 +0100

    Resolviendo conflictos...

--- src/src/Game.cpp
+++ src/src/Game.cpp
@@ -1,12 +1,12 @@
 #include "Game.h"
 
+#include "Action_Manager.h"
 #include "Datos_Partida.h"
 
 #include "Entrada/Input.h"
-#include "IA/Action_Manager.h"
+#include "IA/Decision_Manager.h"
 #include "Personajes/Player.h"
 #include "Interfaz/Interfaz.h"
-#include "Interfaz/Interfaz_Fisica.h"
 #include "Nivel/Nivel.h"
 
 #include <iostream>
@@ -20,15 +20,18 @@ Game* Game::game_instancia(){
 
 	return instancia;
 }
-Game::Game() : _datos(nullptr), _action_manager(nullptr), _interfaz_grafica(nullptr), _interfaz_fisica(nullptr){
+Game::Game() : _datos(nullptr), _action_manager(nullptr), _decision_manager(nullptr), _interfaz_grafica(nullptr){
 
 }
 
 Game::~Game(){
-
 	if(_datos != nullptr) {
 		delete _datos;
 		_datos = nullptr;
+	}
+	if(_decision_manager != nullptr) {
+		delete _decision_manager;
+		_decision_manager = nullptr;
 	}
 	if(_action_manager != nullptr) {
 		delete _action_manager;
@@ -38,33 +41,46 @@ Game::~Game(){
 		delete _interfaz_grafica;
 		_interfaz_grafica = nullptr;
 	}
-	if(_interfaz_fisica != nullptr) {
-		delete _interfaz_fisica;
-		_interfaz_fisica = nullptr;
-	}
 }
 
 
 void Game::crea_partida() {	
 	_interfaz_grafica = Interfaz::Interfaz_getInstance(); //Moose Ninja || 1280 width || 720 height
-	_interfaz_fisica = Interfaz_Fisica::Interfaz_Fisica_GetInstance();
 	_datos = new Datos_Partida();
 	_action_manager = new Action_Manager();
-	//Nivel* _nivel1=new Nivel("Nivel.txt");
+	_decision_manager = new Decision_Manager(_action_manager);
+
+	//cargar nivel 1
+	std::string nivel="txt/Nivel/Nivel1.txt";
+	Nivel* _nivel1=new Nivel(nivel);
+
+	_nivel1->nivel_set_lod(3);
+
+	stack<Vertice*> algo2=_nivel1->nivel_camino_corto_l2(_nivel1->nivel_get_vertice(5),_nivel1->nivel_get_vertice(32));
+	Vertice *pv2;
+                    
+                    while(!algo2.empty()){
+                        pv2=algo2.top();
+                        std::cout << pv2->_id <<" -> ";
+						std::cout << pv2->_lod <<" * ";
+                        algo2.pop();
+                    }
+                    std::cout<<std::endl;
+	std::cout <<_nivel1->nivel_get_nodo(7)->_blackboard->get_zonas_ady()[0] <<std::endl;
+	std::cout << _nivel1->nivel_get_id_vertice(67,27)<<std::endl;
+
 }
 
 void Game::fin_partida() {
-	
 	delete _datos;
+	delete _decision_manager;
 	delete _action_manager;
 	delete _interfaz_grafica;
-	delete _interfaz_fisica;
-
 
 	_datos = nullptr;
+	_decision_manager = nullptr;
 	_action_manager = nullptr;
 	_interfaz_grafica = nullptr;
-	_interfaz_fisica = nullptr;
 }
 
 void Game::run(){
@@ -73,13 +89,10 @@ void Game::run(){
 
 
 void Game::update(double _i_tiempo_desde_ultimo_update){
-	
 	Player *_player = _datos->get_player();
 	_player->update();
 	_player = nullptr;
-	_action_manager->toma_decisiones();
-	_action_manager->realiza_acciones();
-	_interfaz_fisica->update();
+	_decision_manager->toma_decisiones();
 }
 
 void Game::render(float _i_interpolacion){
--- src/src/Main.cpp
+++ src/src/Main.cpp
@@ -3,104 +3,37 @@
 #include "Personajes/Player.h"
 #include "Tiempo/Time.h"
 #include <iostream>
-#include <btBulletDynamicsCommon.h>
 
 const float t_min_IA=(1000/15);
-
-void pruebasNuevas(){
-        btBroadphaseInterface* broadphase = new btDbvtBroadphase();
-
-        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-        btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
-        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-
-        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-
-        dynamicsWorld->setGravity(btVector3(0, -10, 0));
-
-
-        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-
-        btCollisionShape* fallShape = new btSphereShape(1);
-
-
-        btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-        btRigidBody::btRigidBodyConstructionInfo
-                groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-        btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
-        dynamicsWorld->addRigidBody(groundRigidBody);
-
-
-        btDefaultMotionState* fallMotionState =
-                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
-        btScalar mass = 1;
-        btVector3 fallInertia(0, 0, 0);
-        fallShape->calculateLocalInertia(mass, fallInertia);
-        btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
-        btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
-        dynamicsWorld->addRigidBody(fallRigidBody);
-
-
-        for (int i = 0; i < 300; i++) {
-                dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
-                btTransform trans;
-                fallRigidBody->getMotionState()->getWorldTransform(trans);
-
-                std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
-        }
-
-        dynamicsWorld->removeRigidBody(fallRigidBody);
-        delete fallRigidBody->getMotionState();
-        delete fallRigidBody;
-
-        dynamicsWorld->removeRigidBody(groundRigidBody);
-        delete groundRigidBody->getMotionState();
-        delete groundRigidBody;
-
-
-        delete fallShape;
-
-        delete groundShape;
-
-
-        delete dynamicsWorld;
-        delete solver;
-        delete collisionConfiguration;
-        delete dispatcher;
-        delete broadphase;
-}
-
-
 int main(){
-
-	//pruebasNuevas();
 	Game* _juego = Game::game_instancia();
-
-	Time* time=Time::Instance();
+	_juego->crea_partida();
+	/*Time* time=Time::Instance();
 	//double _tiempo_desde_ultimo_update=time->get_current();
 	double _h_ultimo_update=time->get_current();
 	double _interpolacion;
+
 	_juego->crea_partida();
 
 	Time* _time = Time::Instance();
 
 	Interfaz* _interface = Interfaz::Interfaz_getInstance(); //moose ninja || 1280 width || 720 height
-        
+	
+
+	double aa;
+	double bb;
+
 	//_time->updateAntes();
 	while(_interface->Interfaz_getDevice()->run()){
 		//evento para cerrar la ventana
 
+
 		time->set_tiempo_desde_ultimo_update(time->get_current() - _h_ultimo_update);//actualizacion del reloj
 		//std::cout << "Tiempo desde update " << time->get_tiempo_desde_ultimo_update() << std::endl;
 		//update
-                
 		if(time->get_tiempo_desde_ultimo_update()>t_min_IA){
 			_h_ultimo_update=time->get_current();
-                        std::cout << "pre update" << std::endl;
-                        _juego->update(time->get_tiempo_desde_ultimo_update());
-                        std::cout << "post update" << std::endl;
+			_juego->update(time->get_tiempo_desde_ultimo_update());
 		}
 
 
@@ -115,8 +48,7 @@ int main(){
 
 	delete _time;
 
-	delete _juego;
-	
-	return 0;
+	delete _juego;*/
 
+	return 0;
 }
