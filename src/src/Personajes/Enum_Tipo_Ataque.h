4e495f78589286c86908b3c6da04f21866a25489
Pablo Gadea <pablogadea96@gmail.com>
2017-12-19 17:58:04 +0100

    Enum tipo ataque
    Sirve para decir que ataque esta haciendo

---
+++ src/src/Personajes/Enum_Tipo_Ataque.h
@@ -0,0 +1,12 @@
+#ifndef SRC_ENUM_TIPO_ATAQUE_H_
+#define SRC_ENUM_TIPO_ATAQUE_H_
+
+enum Enum_Tipo_Enemigo{
+	Normal,		// Ataque normal
+	Fuerte,		// Ataque fuerte
+	Agarre,		// Agarre que lanza
+	Especial,	// Ataque especial solo para el personaje
+	Desconocido	// Usado para la IA
+};
+
+#endif /* SRC_ENUM_TIPO_ATAQUE_H_ */
