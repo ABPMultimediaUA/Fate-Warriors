004b0f6dd9bd8d86dd1b97a4765fe7c5e251598b
Unknown <sergiomolero5@gmail.com>
2018-01-09 15:25:36 +0100

    Spawn de enemigos preparado

--- src/src/Nivel/Nodo.h
+++ src/src/Nivel/Nodo.h
@@ -11,7 +11,6 @@ public:
 	Nodo(float _i_x, float _i_y, float _i_ancho, float _i_alto, int _i_id, Grafo *_i_grafo, int _i_zona, u_int8_t _i_n_npc);
 	~Nodo();
 	void nodo_crear_objeto(float _i_x, float _i_y, float _i_ancho, float _i_alto, int _i_id);
-	
 
 private:
 
