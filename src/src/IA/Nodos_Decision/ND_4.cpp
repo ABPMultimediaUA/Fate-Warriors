fac4275f25e21269f0b893b4fa1accb396fe1eb0
Unknown <noduorg@gmail.com>
2018-03-01 14:17:51 -0500

    Nodos IA

---
+++ src/src/IA/Nodos_Decision/ND_4.cpp
@@ -0,0 +1,24 @@
+#include "ND_4.h"
+
+#include "../Blackboard.h"
+
+#include<iostream>
+
+ND_4::ND_4(Nodo_Decision& _i_nodo_izq, Nodo_Decision& _i_nodo_der, float _i_valor) : Nodo_Decision(_i_nodo_izq, _i_nodo_der) {
+	_valor = _i_valor;
+}
+
+ND_4::~ND_4() {
+
+}
+
+enum Enum_Acciones ND_4::toma_decision(Blackboard* _blackboard){
+
+    //std::cout <<"Entra ND_4\n";
+	if( _blackboard->_objeto_mas_cerca_esta_cerca == true) { // TRUE, es 1
+        //std::cout <<"_objeto_mas_cerca_esta_cerca\n";
+		return _nodo_der->toma_decision(_blackboard);
+	}
+    //std::cout <<"NO _objeto_mas_cerca_esta_cerca\n";
+	return _nodo_izq->toma_decision(_blackboard);
+}
