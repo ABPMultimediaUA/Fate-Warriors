1604e3c8d03fa96216bd1990ebc6ca77a1830b1e
Unknown <manumeq@gmail.com>
2018-03-24 17:46:41 +0100

    Archivos de interfaz

---
+++ src/src/Interfaz/Interfaz_ME.h
@@ -0,0 +1,109 @@
+#ifndef INTERFAZ_ME_H
+#define INTERFAZ_ME_H
+
+
+#include <cstdint>
+#include <cstdlib>
+#include "iNodoModelado.h"
+#include "iNodoCamara.h"
+#include "../../Camara/Camara.h"
+#include "iNodoLuz.h"
+#include <vector>
+#include "EnumTiposBoundingBox.h"
+#include <btBulletCollisionCommon.h>
+#include "../../Utilidades/Vector.h"
+
+
+class TMooseEngine;
+
+    //primer motor
+class Objeto_ME;
+class Interpolacion;
+class Time;
+class Input;
+class Objeto;
+class DebugDraw;    
+
+    //Bullet
+class btCollisionConfiguration;
+class btBroadphaseInterface;
+class btDispatcher;
+class btConstraintSolver;
+class btDynamicsWorld;
+class btBulletWorldImporter;
+class btPairCachingGhostObject;
+class btRigidBody;
+class btVector3;
+
+class Interfaz_ME{
+
+    public:    
+        ~Interfaz_ME();
+        static Interfaz_ME* get_instancia(uint16_t width, uint16_t height);
+
+        iNodoModelado* crearModelado(const char* ruta);
+        iNodoModelado* crearModelado(const char* ruta, float x, float y, float z);
+        iNodoCamara* crearCamara(bool activa); 
+        iNodoCamara* crearCamara(bool activa, float x, float y, float z);
+        iNodoLuz* crearLuz(bool activa, float intensidad);
+        iNodoLuz* crearLuz(bool activa, float intensidad, float x, float y, float z);
+
+        iNodoModelado* importarEscenario(const char* rutaObj, float x, float y, float z);
+        
+
+        // METODOS DE LA VIEJA INTERFAZ //
+        Interpolacion* crear_interpolacion(float x, float y, float z);
+        btRigidBody* crearRigidBody(Objeto* _i_objeto, BoundingBoxes tipo,const char*  ruta,float x, float y, float z, float _i_peso, iNodoModelado* _i_nodo);
+        void getDimensiones(iNodoModelado* _i_nodo, float &anchura, float &altura, float &profundidad);
+        void crear_ObjetoMotor(Objeto_ME* _i_objeto_me);
+        
+        btCollisionWorld::ClosestRayResultCallback trazaRayo(btVector3 start, btVector3 end, int mascara_colision); //devuelve solo la primera colision del rayo
+        btCollisionWorld::AllHitsRayResultCallback trazaRayoAll(btVector3 start, btVector3 end, int mascara_colision); //devuelve todas las colisiones del rayo
+        btRigidBody* crear_rb_ataque();
+        void setCollisionMask( int mask,  btRigidBody* _i_rigidbody);
+        void setCollisionGroup(int group, btRigidBody* _i_rigidbody);
+
+        void poner_camara_a_entidad(Objeto_ME* _objeto_me);
+        bool x_ve_a_y(Vector3 x, Vector3 y, int mascara_colision); //traza un rayo entre dos posiciones X e Y y comprueba si pueden verse 
+                                         //teniendo en cuenta unicamente el escenario (no el resto de objetos)
+
+        void updateCamaraColision();
+
+        void update(double dt);
+
+        void render();
+        bool ventana_abierta();
+        TMooseEngine* getEngine();
+
+        
+
+    private:
+        static Interfaz_ME* _instancia;
+        TMooseEngine* motor;
+        Interfaz_ME(uint16_t width, uint16_t height);
+        void configuracion_ME(uint16_t width, uint16_t height, bool fullscreen, bool v_sync);
+        void configuracion_bullet();
+        std::vector<iNodo*> lista_i_nodo;
+
+        //COSAS DE LA VIEJA INTERFAZ//
+        std::vector<Objeto_ME*> _objetos_me;
+        uint16_t _id_jugador;
+
+        Camara* camara;
+        //Bullet
+    	btCollisionConfiguration *collisionConfiguration;
+    	btBroadphaseInterface *broadPhase;
+    	btDispatcher *collisionDispatcher;
+    	btConstraintSolver *constraintSolver;
+    	btDynamicsWorld *world;
+    	btBulletWorldImporter* fileLoader;
+        Time* _tiempo;
+        uint8_t _numcubos;
+
+        Vector3* rayOrigen;
+        Vector3* rayDestino;
+
+        double mdt;
+};
+
+#endif
