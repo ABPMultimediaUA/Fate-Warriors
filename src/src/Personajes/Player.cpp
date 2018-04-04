76ec2b2005345cff8254526f700102e22b95d1b3
Unknown <manumeq@gmail.com>
2018-04-04 19:05:42 +0200

    Movimiento relativo a camara arreglado

--- src/src/Personajes/Player.cpp
+++ src/src/Personajes/Player.cpp
@@ -60,7 +60,7 @@ void Player::update(){
 
     if(_input->get_mover(_direccion)){
         // Direccion buena con respecto de la camara
-        uint16_t _direccion_buena = _direccion; //+ Motor::Motor_GetInstance()->angulo_camara();
+        uint16_t _direccion_buena = _direccion + Motor::Motor_GetInstance()->angulo_camara();
         while(_direccion_buena >= 360) _direccion_buena -= 360;
         mover(_direccion_buena);
         //s_sonido->Play_ambiente(2);
