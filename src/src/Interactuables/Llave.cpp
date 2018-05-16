5d01b73cdd2069876cdd5adffde9397701b3c5ec
Unknown <estofunciona96@gmail.com>
2018-05-16 18:32:10 +0200

    Dara coredumped al salir del juego no estan inicializadas a nullptr

--- src/src/Interactuables/Llave.cpp
+++ src/src/Interactuables/Llave.cpp
@@ -9,7 +9,7 @@ Llave::Llave(short _i_id, float _i_x, float _i_y, float _i_z, short _i_id_puerta
    const char* cstr = "Llave";
        
     _objeto_motor = new Objeto_Motor(this,E_BoundingBox, cstr, _i_x,_i_y,_i_z,0);
-    _objeto_visual_mapa = new iObjeto_Mapa(_i_x, _i_z, 50,50, Enum_Llave);
+  //  _objeto_visual_mapa = new iObjeto_Mapa(_i_x, _i_z, 50,50, Enum_Llave);
 }
 
 Llave::~Llave() {
