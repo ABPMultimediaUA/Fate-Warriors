4bd01d93d114a6500e40d375075952e9cf4e500b
Pablo Gadea <pablogadea96@gmail.com>
2018-05-21 14:55:58 +0200

    Funcionalidad interna nuevo menu

---
+++ src/src/Menus/Menu_Configuracion/Submenu_3/Opcion_Texturas.h
@@ -0,0 +1,21 @@
+#ifndef SRC_OPCION_TEXTURAS_H_
+#define SRC_OPCION_TEXTURAS_H_
+
+#include "../../Opcion.h"
+
+class Interfaz_sonido;
+
+class Opcion_Texturas : public Opcion{
+public:
+	Opcion_Texturas(Input* _i_input);
+	virtual ~Opcion_Texturas();
+
+	Opcion* update_opcion(double _i_tiempo, Opcion* _this);
+
+	void set_texturas();
+
+private:
+	uint8_t _texturas;								// De 0 a 2
+};
+
+#endif /* SRC_OPCION_TEXTURAS_H_ */
